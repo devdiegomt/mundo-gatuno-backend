a64e8d0b546a5dc2c7aab1ba309a5c3049897b41
devdiegomt <devdiegomt@gmail.com>
2025-05-05 20:33:29 -0500

    feat: id customizable.

--- models/Product.js
+++ models/Product.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
+  _id: { type: String, required: true },
   title: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String },
