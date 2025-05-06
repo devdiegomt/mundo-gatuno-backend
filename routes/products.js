d00c403aaf34942a68b291dd3c0fe5e0e9ff44ba
devdiegomt <devdiegomt@gmail.com>
2025-05-05 20:22:22 -0500

    fix: fixing environment variable.

--- routes/products.js
+++ routes/products.js
@@ -6,8 +6,12 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   try {
     const products = await Product.find();
+    console.log("Productos encontrados:", products); // Verifica los productos aquí
+    if (products.length === 0)
+      return res.status(404).json({ message: "No hay productos" });
     res.json(products);
   } catch (err) {
+    console.error("Error al obtener productos:", err); // Verifica cualquier error
     res.status(500).json({ message: "Error al obtener productos" });
   }
 });
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import productRoutes from "./routes/products.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
